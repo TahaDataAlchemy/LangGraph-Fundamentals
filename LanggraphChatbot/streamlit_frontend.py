b6bcd5e31b93fe0ba094e81059c77ce98985aef3
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2026-01-06 00:21:44 +0500

    Add files via upload

--- LanggraphChatbot/streamlit_frontend.py
+++ LanggraphChatbot/streamlit_frontend.py
@@ -1,1 +1,23 @@
 import streamlit as st
+
+
+if 'chat_history' not in st.session_state:
+    st.session_state["chat_history"] = []
+
+
+chat_history = []
+for message in st.session_state["chat_history"]:
+    print(message)
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+    with st.chat_message('user'):
+        st.session_state["chat_history"].append({"role":"user","content":user_input})
+        st.text(user_input)
+    
+    with st.chat_message('assistant'):
+        st.session_state["chat_history"].append({"role":"assitant","content":user_input})
+        st.text(user_input)
