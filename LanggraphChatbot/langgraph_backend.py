2cfc611fb4799970e3ce2f607041feada73daf80
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2026-01-07 13:48:25 +0500

    Add files via upload

--- LanggraphChatbot/langgraph_backend.py
+++ LanggraphChatbot/langgraph_backend.py
@@ -5,3 +5,37 @@ from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+if not groq_api_key:
+    raise ValueError("Groq api key not found in environment")
+
+llm = ChatOpenAI(
+    model='openai/gpt-oss-120b',
+    temperature=0,
+    max_tokens = 8192,
+    api_key=groq_api_key,
+    base_url="https://api.groq.com/openai/v1"
+)
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
--- LanggraphChatbot/streamlit_frontend.py
+++ LanggraphChatbot/streamlit_frontend.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
 if 'chat_history' not in st.session_state:
     st.session_state["chat_history"] = []
@@ -18,6 +20,9 @@ if user_input:
         st.session_state["chat_history"].append({"role":"user","content":user_input})
         st.text(user_input)
     
+    response = chatbot.invoke({"messages":[HumanMessage(content=user_input)]},config = CONFIG)
+    ai_message = response["messages"][-1].content
+    print(f"This is response :{response}")
     with st.chat_message('assistant'):
-        st.session_state["chat_history"].append({"role":"assitant","content":user_input})
-        st.text(user_input)
+        st.session_state["chat_history"].append({"role":"assitant","content":ai_message})
+        st.text(ai_message)
