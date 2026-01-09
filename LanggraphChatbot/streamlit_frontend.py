1d96e45b4e6cea680356115b192b0cc0a579cee1
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2026-01-09 17:54:42 +0500

    UI changes

--- LanggraphChatbot/streamlit_frontend.py
+++ LanggraphChatbot/streamlit_frontend.py
@@ -1,11 +1,46 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+import uuid
+
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state["thread_id"] = thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['chat_history'] = []
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state["chat_threads"]:
+        st.session_state['chat_threads'].append(thread_id)
+
 
 if 'chat_history' not in st.session_state:
     st.session_state["chat_history"] = []
 
+
+if "thread_id" not in st.session_state:
+    st.session_state["thread_id"] = generate_thread_id()
+
+if "chat_threads" not in st.session_state:
+    st.session_state["chat_threads"] = []
+
+add_thread(st.session_state["thread_id"])
+
+
+st.sidebar.title("LangGraph Chatbot")
+
+if st.sidebar.button("New Chat"):
+    reset_chat()
+
+st.sidebar.header("My conversations")
+
+for thread_id in st.session_state["chat_threads"]:
+    st.sidebar.button(str(thread_id))
 
 chat_history = []
 for message in st.session_state["chat_history"]:
@@ -19,8 +54,8 @@ if user_input:
     with st.chat_message('user'):
         st.session_state["chat_history"].append({"role":"user","content":user_input})
         st.text(user_input)
-    
-    response = chatbot.invoke({"messages":[HumanMessage(content=user_input)]},config = CONFIG)
+
+    CONFIG = {'configurable': {'thread_id': st.session_state["thread_id"]}}
 
     with st.chat_message("assistant"):
         ai_message = st.write_stream(
